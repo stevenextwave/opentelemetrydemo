f4ed19021773747d456e51e7af6189b981ee589b
stevenextwave <stevenextwave@gmail.com>
2022-04-02 23:22:27 +0800

    Update Main.java

--- be/src/main/java/com/example/sqs/Main.java
+++ be/src/main/java/com/example/sqs/Main.java
@@ -2,29 +2,29 @@ package com.example.sqs;
 
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
-import com.amazonaws.services.sqs.model.AmazonSQSException;
-import com.amazonaws.services.sqs.model.CreateQueueResult;
 import com.amazonaws.services.sqs.model.Message;
-import com.amazonaws.services.sqs.model.SendMessageBatchRequest;
-import com.amazonaws.services.sqs.model.SendMessageBatchRequestEntry;
-import com.amazonaws.services.sqs.model.SendMessageRequest;
-import java.util.Date;
 import java.util.List;
 
 public class Main {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
         String queueUrl= args[0];
+
+        while(true) {
+           
          // receive messages from the queue
          List<Message> messages = sqs.receiveMessage(queueUrl).getMessages();
-
          // delete messages from the queue
          for (Message m : messages) {
              System.out.println(m.getBody());
              sqs.deleteMessage(queueUrl, m.getReceiptHandle());
          }
+          //do your job...
+          Thread.sleep(5 * 1000);
+          
+        }
     }
 }
